86307bd88c7a3fc563e662ba3f789e904b322501
root <root@ip-10-0-0-79.us-west-1.compute.internal>
2019-10-14 20:31:24 +0000

    add profile patches

--- restore_parsers.sh
+++ restore_parsers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source /etc/default/metron
-config_dir=/tmp/save_config_tool
+config_dir=/tmp/restore_config_tool
 
 # pull the latest config files
 sudo /usr/hcp/current/metron/bin/zk_load_configs.sh -m PULL -o $config_dir -z $ZOOKEEPER -f
@@ -10,8 +10,15 @@ sudo /usr/hcp/current/metron/bin/zk_load_configs.sh -m PULL -o $config_dir -z $Z
 cp -r enrichments/$1.json $config_dir/enrichments
 cp -r parsers/$1.json $config_dir/parsers
 cp -r indexing/$1.json $config_dir/indexing
+profiles_copied=false
+if [ ! -f "$config_dir/profiler.json" ]; then
+    cp -r profiles/profiler.json $config_dir
+    profiles_copied=true
+fi 
  
 sudo /usr/hcp/current/metron/bin/zk_load_configs.sh -m PUSH -i $config_dir -z $ZOOKEEPER 
 
-cat profiles/profiler.json | python ../profiler_patch.py | $config_dir/profiler_patch.json
-$METRON_HOME/bin/zk_load_configs.sh -m PATCH -c PROFILER -pf $config_dir/profiler_patch.json -z $ZOOKEEPER  
+if [ "$profiles_copied" = false ]; then
+    cat profiles/profiler.json | python ../profiler_patch.py > $config_dir/profiler_patch.json
+    /usr/hcp/current/metron/bin/zk_load_configs.sh -m PATCH -c PROFILER -pf $config_dir/profiler_patch.json -z $ZOOKEEPER  
+fi
