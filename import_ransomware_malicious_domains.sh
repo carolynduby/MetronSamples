fa7a845d431bfbf91e6c024da6acd6ba5fec261e
Cloud User <centos@mobius.local.localdomain>
2019-11-06 01:48:54 +0000

    add ransomware domain import script

--- import_ransomware_malicious_domains.sh
+++ import_ransomware_malicious_domains.sh
@@ -1,1 +1,3 @@
-curl -s https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt | grep -v  "^#" | grep -v "^$" 
+curl -s https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt | grep -v  "^#" | grep -v "^$" | awk '{print $0 ",ransomwaretracker.abuse.ch"}' > ransomware_domains.csv
+
+./import_csv_enrichments.sh ransomware_domains.csv malicious_domain domain domain,source threatintel 
