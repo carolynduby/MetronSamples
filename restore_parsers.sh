1780d31d3bef136bcfa5611e7fafc9420b348c5f
cduby1 <cduby1@uatasscom028.gsm1900.org>
2019-11-26 16:29:03 -0800

    check if sensor has profiles and the config does not, copy the profiles as is, otherwise patch

--- restore_parsers.sh
+++ restore_parsers.sh
@@ -22,14 +22,21 @@ cp -r enrichments/$1.json $config_dir/enrichments
 cp -r parsers/$1.json $config_dir/parsers
 cp -r indexing/$1.json $config_dir/indexing
 profiles_copied=false
-if [ ! -f "$config_dir/profiler.json" ]; then
-    cp -r profiles/profiler.json $config_dir
-    profiles_copied=true
-fi 
+install_profiles=false
+if [ -f "profiles/profiler.json" ]; then
+    install_profiles=true
+fi
+     
+if [ "$install_profiles" = true ]; then
+   if [ ! -f "$config_dir/profiler.json" ]; then
+        cp -r profiles/profiler.json $config_dir
+        profiles_copied=true
+   fi 
+fi
  
 /usr/hcp/current/metron/bin/zk_load_configs.sh -m PUSH -i $config_dir -z $ZOOKEEPER 
 
-if [ "$profiles_copied" = false ]; then
+if [ "$profiles_copied" = false -a "$install_profiles" = true ]; then
     cat profiles/profiler.json | python ../profiler_patch.py > $config_dir/profiler_patch.json
     /usr/hcp/current/metron/bin/zk_load_configs.sh -m PATCH -c PROFILER -pf $config_dir/profiler_patch.json -z $ZOOKEEPER  
 fi
